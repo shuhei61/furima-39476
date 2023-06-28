6503d7ce1c801ffae10ecf3d900965d935e5ad27
shuhei61 <135288191+shuhei61@users.noreply.github.com>
2023-06-28 20:24:30 +0900

    rubocopで修正。rubocopで引っかかったのでまだ使用しないuserコントローラーは削除。

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,16 +3,16 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  
+
   with_options presence: true do
     validates :nickname
-    validates :lastname, format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "is invalid. Input full-width characters."}
-    validates :firstname, format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "is invalid. Input full-width characters."}
-    validates :lastname_kana, format: {with: /\A[ァ-ヶー]+\z/, message: "is invalid. Input full-width katakana characters."}
-    validates :firstname_kana, format: {with: /\A[ァ-ヶー]+\z/, message: "is invalid. Input full-width katakana characters."}
+    validates :lastname, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'is invalid. Input full-width characters.' }
+    validates :firstname, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'is invalid. Input full-width characters.' }
+    validates :lastname_kana, format: { with: /\A[ァ-ヶー]+\z/, message: 'is invalid. Input full-width katakana characters.' }
+    validates :firstname_kana, format: { with: /\A[ァ-ヶー]+\z/, message: 'is invalid. Input full-width katakana characters.' }
     validates :birth_date
   end
 
-  validates :password, format: {with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: "is invalid. Include both letters and numbers"}
-
+  validates :password,
+            format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i, message: 'is invalid. Include both letters and numbers' }
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -14,12 +14,12 @@ RSpec.describe User, type: :model do
 
     context '新規登録ができない' do
       it 'ニックネームが必須であること' do
-        @user.nickname = ""
+        @user.nickname = ''
         @user.valid?
         expect(@user.errors.full_messages).to include("Nickname can't be blank")
       end
       it 'メールアドレスが必須であること' do
-        @user.email = ""
+        @user.email = ''
         @user.valid?
         expect(@user.errors.full_messages).to include("Email can't be blank")
       end
@@ -28,72 +28,71 @@ RSpec.describe User, type: :model do
         new_user = FactoryBot.build(:user)
         new_user.email = @user.email
         new_user.valid?
-        expect(new_user.errors.full_messages).to include("Email has already been taken")
+        expect(new_user.errors.full_messages).to include('Email has already been taken')
       end
       it 'メールアドレスは、@を含む必要があること' do
-        @user.email = "mail.com"
+        @user.email = 'mail.com'
         @user.valid?
-        expect(@user.errors.full_messages).to include("Email is invalid")
+        expect(@user.errors.full_messages).to include('Email is invalid')
       end
       it 'パスワードが必須であること' do
-        @user.password = ""
+        @user.password = ''
         @user.valid?
         expect(@user.errors.full_messages).to include("Password can't be blank")
       end
       it 'パスワードは、6文字以上での入力が必須であること' do
-        @user.password = "one11"
+        @user.password = 'one11'
         @user.valid?
-        expect(@user.errors.full_messages).to include("Password is too short (minimum is 6 characters)")
+        expect(@user.errors.full_messages).to include('Password is too short (minimum is 6 characters)')
       end
       it 'パスワードは、半角英数字混合での入力が必須であること' do
-        @user.password = "111111"
+        @user.password = '111111'
         @user.valid?
-        expect(@user.errors.full_messages).to include("Password is invalid. Include both letters and numbers")
+        expect(@user.errors.full_messages).to include('Password is invalid. Include both letters and numbers')
       end
       it 'パスワードとパスワード（確認）は、値の一致が必須であること' do
-        @user.password = "one111"
-        @user.password_confirmation = "one112"
+        @user.password = 'one111'
+        @user.password_confirmation = 'one112'
         @user.valid?
         expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
       end
       it 'お名前(全角)は、名字が必須であること' do
-        @user.lastname = ""
+        @user.lastname = ''
         @user.valid?
         expect(@user.errors.full_messages).to include("Lastname can't be blank")
       end
       it 'お名前(全角)は、名前が必須であること' do
-        @user.firstname = ""
+        @user.firstname = ''
         @user.valid?
         expect(@user.errors.full_messages).to include("Firstname can't be blank")
       end
       it 'お名前(全角)は、全角（漢字・ひらがな・カタカナ）での入力が必須であること' do
-        @user.lastname = "山田"
-        @user.firstname = "ﾀﾛｳ"
+        @user.lastname = '山田'
+        @user.firstname = 'ﾀﾛｳ'
         @user.valid?
-        expect(@user.errors.full_messages).to include("Firstname is invalid. Input full-width characters.")
+        expect(@user.errors.full_messages).to include('Firstname is invalid. Input full-width characters.')
       end
       it 'お名前カナ(全角)は、名字が必須であること' do
-        @user.lastname_kana = ""
+        @user.lastname_kana = ''
         @user.valid?
         expect(@user.errors.full_messages).to include("Lastname kana can't be blank")
       end
       it 'お名前カナ(全角)は、名前が必須であること' do
-        @user.firstname_kana = ""
+        @user.firstname_kana = ''
         @user.valid?
         expect(@user.errors.full_messages).to include("Firstname kana can't be blank")
       end
       it 'お名前カナ(全角)は、全角（カタカナ）での入力が必須であること' do
-        @user.lastname = "ヤマダ"
-        @user.firstname = "ﾀﾛｳ"
+        @user.lastname = 'ヤマダ'
+        @user.firstname = 'ﾀﾛｳ'
         @user.valid?
-        expect(@user.errors.full_messages).to include("Firstname is invalid. Input full-width characters.")
+        expect(@user.errors.full_messages).to include('Firstname is invalid. Input full-width characters.')
       end
       it '生年月日が必須であること' do
-        @user.birth_date = ""
+        @user.birth_date = ''
         @user.valid?
         expect(@user.errors.full_messages).to include("Birth date can't be blank")
       end
-
     end
   end
 end
