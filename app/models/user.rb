7f19d3ce5abd065b70016c892a47c27a2a5758a0
shuhei61 <135288191+shuhei61@users.noreply.github.com>
2023-06-28 12:21:43 +0900

    userモデルにバリデーション

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,15 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  with_options presence: true do
+    validates :nickname
+    validates :lastname
+    validates :firstname
+    validates :lastname_kana
+    validates :firstname_kana
+    validates :birth_date
+  end
+
+  
 end
