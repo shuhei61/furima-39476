9fb7b0004ec1a77d43ce081efb1502709e5b0402
shuhei61 <135288191+shuhei61@users.noreply.github.com>
2023-06-29 17:23:33 +0900

    activestorageの導入、バリデーションとアソシエーション

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,16 +3,19 @@ class Item < ApplicationRecord
   with_options presence: true do
     validates :nickname
     validates :description
-    validates :price
+    validates :price, format: {with: /\A[3-9]\d{2,6}\Z/, message: "Price is out of setting range"}
   end
 
-  validates :category_id, numericality: { other_than: 1 }
-  validates :state_id, numericality: { other_than: 1 }
-  validates :shipcharge_id, numericality: { other_than: 1 }
-  validates :prefecture_id, numericality: { other_than: 1 }
-  validates :shipday_id, numericality: { other_than: 1 }
+  with_options numericality: { other_than: 1 } do
+    validates :category_id
+    validates :state_id
+    validates :shipcharge_id
+    validates :prefecture_id
+    validates :shipday_id
+  end
 
   belongs_to :user
+  has_one_attached :image
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
