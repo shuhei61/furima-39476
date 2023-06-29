126e3ff0a357eccdcd7c5b83caccabf0c1569a79
shuhei61 <135288191+shuhei61@users.noreply.github.com>
2023-06-29 19:25:33 +0900

    エラーメッセージが存在しているか確認のため、単体テストを少し作成

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @item = FactoryBot.build(:item)
+  end
+
+  describe '出品登録' do
+    context '登録できるとき' do
+      it '必要な情報を適切に入力して「出品」ボタンを押すと、新規登録ができる' do
+        expect(@item).to be_valid
+      end
+    end
+
+    context '登録できないとき' do
+      it '商品画像を1枚つけることが必須である' do
+      end
+      it '商品名が必須である' do
+        @item.name = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Name can't be blank")
+      end
+    end
+  end
 end
