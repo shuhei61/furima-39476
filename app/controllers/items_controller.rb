5dae641b6d065d9fa748c08b577f72150552ecbf
shuhei61 <135288191+shuhei61@users.noreply.github.com>
2023-06-30 11:21:13 +0900

    ログインの有無で出品ページに遷移できる

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,7 @@
 class ItemsController < ApplicationController
+
+  before_action :move_to_session, only: [:new]
+
   def index
   end
 
@@ -19,4 +22,11 @@ class ItemsController < ApplicationController
   def item_create_params
     params.require(:item).permit(:image, :name, :description, :category_id, :state_id, :shipcharge_id, :prefecture_id, :shipday_id, :price).merge(user_id: current_user.id)
   end
+
+  def move_to_session
+    unless user_signed_in?
+      redirect_to new_user_session_path
+    end
+  end
+
 end
