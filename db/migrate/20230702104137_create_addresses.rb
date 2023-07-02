bfa6474eb95dee9b26a18dd25efe41e81b066f11
shuhei61 <135288191+shuhei61@users.noreply.github.com>
2023-07-02 19:51:30 +0900

    orderとaddressテーブル作成

--- db/migrate/20230702104137_create_addresses.rb
+++ db/migrate/20230702104137_create_addresses.rb
@@ -1,7 +1,13 @@
 class CreateAddresses < ActiveRecord::Migration[7.0]
   def change
     create_table :addresses do |t|
-
+      t.string :post_code, null: false
+      t.integer :prefecture_id, null: false
+      t.string :municipalities, null: false
+      t.string :house_number, null: false
+      t.string :building_name, null: false
+      t.string :phone_number, null: false
+      t.references :order, null: false, foreign_key: true
       t.timestamps
     end
   end
