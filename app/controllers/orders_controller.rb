9356aa825bd3e679e62e5f7d102b03e872ee93be
shuhei61 <135288191+shuhei61@users.noreply.github.com>
2023-07-02 21:30:37 +0900

    orderコントローラーにcreate関連の記述

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,6 +1,23 @@
 class OrdersController < ApplicationController
 
   def index
+    @order_address = OrderAddress.new
   end
-  
+
+  def create
+    @order_address = OrderAddress.new(create_orderaddress_params)
+    if @order_address.valid?
+      @order_address.save
+      redirect_to "/"
+    else
+      render :index, status: :unprocessable_entity
+    end
+
+  end
+
+  private
+  def create_orderaddress_params
+    params.require(:order_address).permit(:post_code, :prefecture_id, :municipalities, :house_number, :building_name, :phone_number, :item_id).merge(user_id: current_user.id)
+  end
+
 end
