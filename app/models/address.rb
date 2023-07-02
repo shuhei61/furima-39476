ae5593d7f3caf9a96869cda6197e7f3ce958fb18
shuhei61 <135288191+shuhei61@users.noreply.github.com>
2023-07-02 20:24:01 +0900

    addressモデルにアソシエーションとバリデーション

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,2 +1,31 @@
 class Address < ApplicationRecord
+
+  with_options presence: true do
+    validates :post_code
+    validates :municipalities
+    validates :house_number
+    validates :building_name
+    validates :phone_number, format: { with: /\A\d+\Z/, message: 'is invalid. Input only number.' }
+  end
+
+  validates :phone_number, length: { minimum: 10, maximum: 11 }
+  validate :phone_length_judge
+
+  with_options numericality: { other_than: 1, message: "can't be blank" } do
+    validates :prefecture_id
+  end
+
+  belongs_to :order
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :prefecture
+
+  def phone_length_judge
+    if phone_number.present? && phone_number.length < 10
+      errors.add(:phone_number, 'Phone number is too short')
+    elsif phone_number.present? && phone_number.length > 11
+      errors.add(:phone_number, 'Phone number is too long')
+    end
+  end
+
 end
