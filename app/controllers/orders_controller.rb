79e4d99cbbffbd54cf3d743877dfbf2fe35f4318
shuhei61 <135288191+shuhei61@users.noreply.github.com>
2023-07-03 11:01:07 +0900

    payjp関連初期設定

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,12 +1,17 @@
 class OrdersController < ApplicationController
 
   def index
+    gon.public_key = ENV["PAYJP_PUBLIC_KEY"]
     @order_address = OrderAddress.new
   end
 
   def create
     @order_address = OrderAddress.new(create_orderaddress_params)
     if @order_address.valid?
+      Payjp::Charge.create(
+        amount: order_params[:price],  # 商品の値段
+        card: order_params[:token],
+        currency: 'jpy'                 # 通貨の種類（日本円）)
       @order_address.save
       redirect_to "/"
     else
@@ -17,7 +22,7 @@ class OrdersController < ApplicationController
 
   private
   def create_orderaddress_params
-    params.require(:order_address).permit(:post_code, :prefecture_id, :municipalities, :house_number, :building_name, :phone_number, :item_id).merge(user_id: current_user.id)
+    params.require(:order_address).permit(:post_code, :prefecture_id, :municipalities, :house_number, :building_name, :phone_number, :item_id).merge(user_id: current_user.id, token: params[:token])
   end
 
 end
